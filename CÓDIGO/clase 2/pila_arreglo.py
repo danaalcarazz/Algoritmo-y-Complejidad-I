674211c587f62951779f466a1e36b7ac652ead2d
danaalcarazz <danaalcaraz133@gmail.com>
2026-08-24 12:25:01 -0300

    fix(clase-2): Reemplazar código corregido

--- CÓDIGO/clase 2/pila_arreglo.py
+++ CÓDIGO/clase 2/pila_arreglo.py
@@ -1,20 +1,29 @@
-from array import array
-
 class PilaArreglo:
-    def __init__(self):
-        self.__datos = array('i')
+    def __init__(self, capacidad_inicial=10):
+        self.__capacidad = capacidad_inicial
+        self.__datos = [None] * self.__capacidad
         self.__tope = -1
 
+    def __redimensionar(self, nueva_capacidad):
+        nuevos_datos = [None] * nueva_capacidad
+        for i in range(self.__tope + 1):
+            nuevos_datos[i] = self.__datos[i]
+        self.__datos = nuevos_datos
+        self.__capacidad = nueva_capacidad
+
     def apilar(self, elemento):
-        self.__datos.append(elemento)
+        if self.__tope + 1 == self.__capacidad:
+            self.__redimensionar(self.__capacidad * 2)
+        
         self.__tope += 1
+        self.__datos[self.__tope] = elemento
 
     def desapilar(self):
         if self.estaVacia():
             raise Exception("No se puede desapilar una pila vacía")
 
         elemento = self.__datos[self.__tope]
-        self.__datos.pop()
+        self.__datos[self.__tope] = None  # Limpia la referencia
         self.__tope -= 1
 
         return elemento
